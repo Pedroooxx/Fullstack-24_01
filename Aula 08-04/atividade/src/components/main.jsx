53a05b56b26bb3dbc3c2874c5bce699b4212e127
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-04-15 00:34:19 -0300

    creating react body

---
+++ Aula 08-04/atividade/src/components/main.jsx
@@ -0,0 +1,14 @@
+import main from "./stylesheets/main.css"
+
+const Main = () => {
+
+
+
+    return (
+      <div className="div">
+        <h1>Text</h1>
+      </div>
+    );
+  };
+  
+  export default Main;
